c5407c7d694f12b19cf7000253da86b743e63418
Melissa Ferenal <mileesah@gmail.com>
2018-02-03 11:35:37 +1100

    Add initial classes

---
+++ src/main/java/au/com/afl/exercise/Game.java
@@ -0,0 +1,11 @@
+package au.com.afl.exercise;
+
+
+public class Game
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Let's try playing a counting game!" );
+        System.out.println( "Have Fun" );
+    }
+}
